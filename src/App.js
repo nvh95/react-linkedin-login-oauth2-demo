9f95827684314f89c1e5f38374f47ef9cb8da575
Hung Viet Nguyen <henry@gotitapp.co>
2021-10-30 02:52:48 +0700

    feat: use first version 2.x alpha 🎉

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { LinkedInPopUp } from 'react-linkedin-login-oauth2';
-import LinkedInPage from './LinkedInPage';
+import { LinkedInCallback } from "react-linkedin-login-oauth2";
+import LinkedInPage from "./LinkedInPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch >
-        <Route exact path="/linkedin" component={LinkedInPopUp} />
+      <Switch>
+        <Route exact path="/linkedin" component={LinkedInCallback} />
         <Route path="/" component={LinkedInPage} />
       </Switch>
     </BrowserRouter>
--- src/LinkedInPage.js
+++ src/LinkedInPage.js
@@ -1,50 +1,44 @@
-import React, { Component } from 'react';
+import React from "react";
 
-import { LinkedIn } from 'react-linkedin-login-oauth2';
-import linkedin from 'react-linkedin-login-oauth2/assets/linkedin.png'
+import { useLinkedIn } from "react-linkedin-login-oauth2";
+import linkedin from "react-linkedin-login-oauth2/assets/linkedin.png";
 
-class LinkedInPage extends Component {
-  state = {
-    code: '',
-    errorMessage: '',
-  };
+function LinkedInPage() {
+  const { linkedInLogin } = useLinkedIn({
+    clientId: "86vhj2q7ukf83q",
+    redirectUri: "http://localhost:3000/linkedin",
+    onSuccess: (code) => {
+      console.log(code);
+      setCode(code);
+    },
+    onFailure: (error) => {
+      console.log(error);
+      setErrorMessage(error.errorMessage);
+    },
+  });
+  const [code, setCode] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
 
+  return (
+    <div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          linkedInLogin();
+        }}
+      >
+        <img
+          src={linkedin}
+          alt="Log in with Linked In"
+          style={{ maxWidth: "180px" }}
+        />
+      </button>
 
-  handleSuccess = (data) => {
-    this.setState({
-      code: data.code,
-      errorMessage: '',
-    });
-  }
-
-  handleFailure = (error) => {
-    this.setState({
-      code: '',
-      errorMessage: error.errorMessage,
-    });
-  }
-  render() {
-    const { code, errorMessage } = this.state;
-    return (
-      <div>
-        <LinkedIn
-          clientId="81lx5we2omq9xh"
-          redirectUri={`${window.location.origin}/linkedin`}
-          scope="r_emailaddress"
-          state="34232423"
-          onFailure={this.handleFailure}
-          onSuccess={this.handleSuccess}
-          supportIE
-          redirectPath='/linkedin'
-        >
-          <img src={linkedin} alt="Log in with Linked In" style={{ maxWidth: '180px' }} />
-        </LinkedIn>
-        {!code && <div>No code</div>}
-        {code && <div>Code: {code}</div>}
-        {errorMessage && <div>{errorMessage}</div>}
-      </div>
-    );
-  }
+      {!code && <div>No code</div>}
+      {code && <div>Code: {code}</div>}
+      {errorMessage && <div>{errorMessage}</div>}
+    </div>
+  );
 }
 
 export default LinkedInPage;
