e72bdc5679663a815c6ffcf818aaaecdeedd910b
Hung Viet Nguyen <henry@gotitapp.co>
2021-10-31 23:45:12 +0700

    feat: update to 2.0.0 and add reference when get the code successfully

--- src/LinkedInPage.js
+++ src/LinkedInPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styled from "styled-components";
 import { useLinkedIn } from "react-linkedin-login-oauth2";
 import linkedin from "react-linkedin-login-oauth2/assets/linkedin.png";
 
@@ -10,8 +10,10 @@ function LinkedInPage() {
     onSuccess: (code) => {
       console.log(code);
       setCode(code);
+      setErrorMessage("");
     },
-    onFailure: (error) => {
+    scope: "r_liteprofile r_emailaddress",
+    onError: (error) => {
       console.log(error);
       setErrorMessage(error.errorMessage);
     },
@@ -20,25 +22,46 @@ function LinkedInPage() {
   const [errorMessage, setErrorMessage] = React.useState("");
 
   return (
-    <div>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          linkedInLogin();
-        }}
-      >
-        <img
-          src={linkedin}
-          alt="Log in with Linked In"
-          style={{ maxWidth: "180px" }}
-        />
-      </button>
+    <Wrapper>
+      <img
+        onClick={linkedInLogin}
+        src={linkedin}
+        alt="Log in with Linked In"
+        style={{ maxWidth: "180px" }}
+      />
 
       {!code && <div>No code</div>}
-      {code && <div>Code: {code}</div>}
+      {code && (
+        <div>
+          <div>Authorization Code: {code}</div>
+          <div>
+            Follow{" "}
+            <Link
+              target="_blank"
+              href="https://docs.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?context=linkedin%2Fconsumer%2Fcontext&tabs=HTTPS#step-3-exchange-authorization-code-for-an-access-token"
+              rel="noreferrer"
+            >
+              this
+            </Link>{" "}
+            to continue
+          </div>
+        </div>
+      )}
       {errorMessage && <div>{errorMessage}</div>}
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const Link = styled.a`
+  font-size: 20px;
+  font-weight: bold;
+`;
 
 export default LinkedInPage;
