e26bd634e61bc44b77408355ca5fdcc59aac073f
Madhav Milan <madhavcoolmania@gmail.com>
2022-10-18 21:03:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { LinkedInCallback } from "react-linkedin-login-oauth2";
 import LinkedInPage from "./LinkedInPage";
 
 function App() {
+  const typeOfLogin = ["GMAIL", "LINKEDIN"];
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/linkedin" component={LinkedInCallback} />
-        <Route path="/" component={LinkedInPage} />
+        <Route path="/" render={() =><LinkedInPage typeOfLogin={typeOfLogin} />} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/GoogleLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function GoogleLogin(){
+    return(
+        <>
+        <h1>Hello</h1>
+        </>
+    )
+}
+export default GoogleLogin;
